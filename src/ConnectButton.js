b7c4059c8386155f628f4b62aa071f01efd33e19
Linus Weigand <linusweigand@protonmail.com>
2022-05-26 11:53:44 +0200

    fix connectButton

--- src/ConnectButton.js
+++ src/ConnectButton.js
@@ -2,18 +2,23 @@ import React from 'react'
 import { Button } from "@material-ui/core";
 import AccountBalanceWalletIcon from '@mui/icons-material/AccountBalanceWallet';
 import { useSelector } from "react-redux";
-import { selectAddress, selectConnected } from "./features/userSlice";
+import { login, selectAddress } from "./features/userSlice";
+import { useDispatch } from "react-redux";
 
 function ConnectButton() {
   const address = useSelector(selectAddress);
-  const connected = useSelector(selectConnected);
+  const dispatch = useDispatch();
+
+  const setAddress = (address) => {
+    dispatch(login(address));
+  }
   return (
     <div>
       <Button 
         className="header__connectWallet"
         startIcon={ <AccountBalanceWalletIcon fontSize="large"
         /> }>
-          { connected ? {address} : "Connect Wallet" }
+          { address ? {address} : "Connect Wallet" }
       </Button>
     </div>
   )
