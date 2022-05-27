9cecaa2dfb0b9a546ef63728264315f245bc8d97
Linus Weigand <linusweigand@protonmail.com>
2022-05-27 11:27:00 +0200

    wallet connect

--- src/features/userSlice.js
+++ src/features/userSlice.js
@@ -1,34 +1,26 @@
 import { createSlice } from '@reduxjs/toolkit';
-// import { fetchCount } from '. /counterAPI';
 
 const initialState = {
-  address: null
+  address: null,
+  connected: false
 };
 
 
 export const userSlice = createSlice({
   name: "user",
   initialState,
-  // The `reducers` field lets us define reducers and generate associated actions
   reducers: {
-    login: (state, action) => {
-      // Redux Toolkit allows us to write "mutating" logic in reducers. It
-      // doesn't actually mutate the state because it uses the Immer library,
-      // which detects changes to a "draft state" and produces a brand new
-      // immutable state based off those changes
+    setAddress: (state, action) => {
       state.address = action.payload;
+    },
+    setConnected: (state, action) => {
+      state.connected = action.payload;
     }
   },
 });
 
-export const { login } = userSlice.actions;
+export const { setAddress, setConnected } = userSlice.actions;
 
-// The function below is called a selector and allows us to select a value from
-// the state. Selectors can also be defined inline where they're used instead of
-// in the slice file. For example: `useSelector((state: RootState) => state.counter.value)`
 export const selectAddress = (state) => state.address;
-
-// We can also write thunks by hand, which may contain both sync and async logic.
-// Here's an example of conditionally dispatching actions based on current state.
 
 export default userSlice.reducer;
--- src/pages/ConnectButton/ConnectButton.js
+++ src/pages/ConnectButton/ConnectButton.js
@@ -1,25 +1,88 @@
-import React from 'react'
+import React, { useState, useEffect, useCallback } from "react";
 import { Button } from "@material-ui/core";
 import AccountBalanceWalletIcon from '@mui/icons-material/AccountBalanceWallet';
 import { useSelector } from "react-redux";
-import { login, selectAddress } from "../../features/userSlice";
+import { setAddress, setConnected} from "../../features/userSlice";
 import { useDispatch } from "react-redux";
 
+
+
 function ConnectButton() {
-  const address = useSelector(selectAddress);
+  const user = useSelector(state => state.user);
   const dispatch = useDispatch();
 
-  const setAddress = (address) => {
-    dispatch(login(address));
+
+  const checkIfWalletIsConnected = async () => {
+    try {
+      const { ethereum } = window;
+
+      if (!ethereum) {
+        console.log("Make sure  you have metamask!");
+        return;
+      } else {
+        console.log("We have the ethereum object", ethereum);
+      }
+
+      const accounts = await ethereum.request({ method: 'eth_accounts'});
+
+      if (accounts.length !== 0) {
+        const account = accounts[0];
+        console.log("Found an authorized account: ", account);
+        dispatch(setAddress(account));
+        dispatch(setConnected(true));
+      } else {
+        console.log("No authorized account found");
+      }
+    } catch (error) {
+      console.error(error);
+    }
   }
+
+  const connectWallet = async () => {
+    try {
+      const { ethereum } = window;
+
+      if (!ethereum) {
+        alert("Get Metamask!");
+        return;
+      }
+
+      const accounts = await ethereum.request({ method: "eth_requestAccounts"});
+
+      console.log("Connected", accounts[0]);
+      dispatch(setAddress(accounts[0]));
+      dispatch(setConnected(true));
+    } catch(error) {
+      console.error(error);
+    }
+  }
+
+  useEffect(() => {
+    checkIfWalletIsConnected();
+  }, []);
+
   return (
     <div>
-      <Button 
-        className="header__connectWallet"
-        startIcon={ <AccountBalanceWalletIcon fontSize="large"
-        /> }>
-          { address ? {address} : "Connect Wallet" }
-      </Button>
+      
+      { user.connected ? 
+          (<div className="bio">
+            <Button 
+            className="header__connectWallet"
+            startIcon={ <AccountBalanceWalletIcon fontSize="large"
+            /> }
+            >
+              {user.address.substring(0, 5) + "..." + user.address.substring(user.address.length - 4, user.address.length)}
+          </Button> 
+          </div>) : 
+          ( <Button 
+            className="header__connectWallet"
+            startIcon={ <AccountBalanceWalletIcon fontSize="large"
+            /> }
+            onClick={connectWallet}
+            >
+              Connect Wallet
+          </Button>)
+        }
     </div>
   )
 }
